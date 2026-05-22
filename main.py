10331d6067687d0c064cf0986c865ea767eb4aa2
jorie306 <leziana6@gmail.com>
2026-05-22 18:28:58 +0700

    Integrate login and enhance bot commands
    Added login functionality and improved start message.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 
 from pyrogram import Client, filters
 
+from login import load_login
+
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
@@ -13,20 +15,37 @@ app = Client(
     bot_token=BOT_TOKEN
 )
 
+# =========================================
+# LOAD LOGIN
+# =========================================
+
+load_login(app)
+
+# =========================================
+# START
+# =========================================
+
 @app.on_message(filters.command("start"))
 async def start(client, message):
 
     await message.reply_text(
-        "BOT HIDUP"
+        """
+🤖 STOREBOT ACTIVE
+
+/login = login userbot
+/ping = test bot
+"""
     )
+
+# =========================================
+# PING
+# =========================================
 
 @app.on_message(filters.command("ping"))
 async def ping(client, message):
 
-    print("PING MASUK")
-
     await message.reply_text(
-        "PONG"
+        "🏓 PONG"
     )
 
 print("🚀 BOT ONLINE")
